cc1d51b1327bdeca18da4dbd3cfd6ceef7cfb0c4
Nicholas Walton <cactar65@gmail.com>
2019-09-30 05:24:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,8 +16,8 @@ end
 def monopoly_with_third_tier
     monopoly = {
       :railroads => {
-        :pieces => 4,
-        :rent_in_dollars =>
+        :pieces=>4,
+        :rent_in_dollars=>
       },
       
     }
