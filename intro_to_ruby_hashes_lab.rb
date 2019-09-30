9c6b572fd60ba65a718c8689cdb89023afb143f7
Nicholas Walton <cactar65@gmail.com>
2019-09-30 05:40:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,9 @@ def monopoly_with_third_tier
           :four_pieces_owned=>200
         },
         :names=> {
-          :reading_railroads=> {}
+          :reading_railroad=> {}
+          :pennsylvania_railroad=> {}
+          
         }
       }
       
